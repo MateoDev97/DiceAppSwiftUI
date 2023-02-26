b6ae36aa750d9c632f7b9cb7b66ef5864338173e
Mateo Ortiz <mateo972000@gmail.com>
2023-02-26 12:05:08 -0500

    Initial Commit

---
+++ DiceeSwiftUI/DiceeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwiftUIApp.swift
+//  DiceeSwiftUI
+//
+//  Created by Mateo Ortiz on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
